536f0f63fab6786f45d78bd0b5716dfa2a1c4039
Luis Gonzalez <luisgonzalez@Mac.lan>
2026-05-26 14:48:49 -0400

    add Codex Ambassador and security updates

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,12 @@ export default defineConfig({
   // Custom domains are served from the site root.
   base: '/',
   build: {
-    minify: 'esbuild',
     cssMinify: true,
     rollupOptions: {
       output: {
         manualChunks: undefined,
       },
     },
-  },
-  esbuild: {
-    drop: ['console', 'debugger'],
   },
   server: {
     open: true,
